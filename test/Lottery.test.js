14ba9644bc1d5df2c46ae28d871d47d9eff51203
Leonardo Souza <leonardocsouza@gmail.com>
2018-03-13 20:42:32 -0400

    fix test and assert one player can enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,7 +1,14 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+const provider = ganache.provider();
+
+// having a constructor to create a separate instance
+// allows us to connect to more than one ethereum network
+// from the same project (though this isn't that common)
+// lowercase first letter (w) because this is the instance
+const web3 = new Web3(provider);
 
 const { interface, bytecode } = require('../compile');
 
@@ -14,10 +21,28 @@ beforeEach(async () => {
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: '1000000' })
+
+  // this is needed or it doesn't work
+  // (some tutorial in part 1 explained it)
+  lottery.setProvider(provider);
 });
 
 describe('Lottery Contract', () => {
   it('deploys a contract', () => {
     assert.ok(lottery.options.address);
+  });
+
+  it('allows one account to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+
+    assert.equal(accounts[0], players[0]);
+    assert.equal(1, players.length);
   });
 });
