ac8f82551735744ce9c6eb4eeb177838993c2572
Leonardo Souza <leonardocsouza@gmail.com>
2018-03-20 09:23:53 -0400

    add logging of interface during deploy

--- deploy.js
+++ deploy.js
@@ -24,7 +24,7 @@ const deploy = async () => {
     .deploy({ data: bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
-  // Log address of contract deployment
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
